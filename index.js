f933ff7269c33db921ef5385ce2ab4b3496a9240
Ricardo F Pereira <ricardofonseca_1@hotmail.com>
2024-02-02 16:48:44 -0300

    Starting requisitions of User

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const userRoute = require('./src/routes/user.route')
 
 const app = express()
 
-app.use("/home", userRoute)
+const port = 3000;
+app.use(express.json())
+app.use("/user", userRoute)
 
 
 
-app.listen(3000)
+app.listen(port, () =>console.log(`Servidor rodando da porta ${port}`))
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,20 @@
-const start = (req, res) =>{
-res.send('Start Application')
+const create = (req, res) =>{
+const {name,username,email,password} = req.body
+
+if(!name || !username || !email || !password){
+return res.status(400).send({message:"Submit all fields for registration"})
 }
 
-module.exports = {start}
+
+res.status(201).send({
+    message:"User created successfully",
+    user:{
+        name, username, email 
+    },
+
+   
+})
+
+}
+
+module.exports = {create}
